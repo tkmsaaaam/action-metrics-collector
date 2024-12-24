e2d633adbe173577c58dde8130ea2d790ca6cfac
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2024-12-24 23:22:26 +0900

    feat: add test cases

--- main_test.go
+++ main_test.go
@@ -14,6 +14,9 @@ func TestMakeMap(t *testing.T) {
 	b := 0 * time.Second
 	c := time.Date(2017, 12, 1, 8, 52, 40, 0, time.Local)
 	d := 10 * time.Second
+	e := time.Date(2017, 12, 1, 8, 52, 50, 0, time.Local)
+	f := time.Date(2017, 12, 1, 8, 53, 0, 0, time.Local)
+	g := 30 * time.Second
 
 	tests := []struct {
 		name   string
@@ -25,10 +28,15 @@ func TestMakeMap(t *testing.T) {
 			apiRes: &slack.GetConversationHistoryResponse{Messages: []slack.Message{{Msg: slack.Msg{Text: "test", Timestamp: "1512085950.000000"}}}},
 			want:   map[string]*Result{"test": {Details: []*Detail{{t: &a, diff: &b}}, sum: &b}},
 		},
-				{
+		{
 			name:   "two events",
 			apiRes: &slack.GetConversationHistoryResponse{Messages: []slack.Message{{Msg: slack.Msg{Text: "test", Timestamp: "1512085950.000000"}}, {Msg: slack.Msg{Text: "test", Timestamp: "1512085960.000000"}}}},
 			want:   map[string]*Result{"test": {Details: []*Detail{{t: &a, diff: &b}, {t: &c, diff: &d}}, sum: &d}},
+		},
+		{
+			name:   "Multiple events of multiple types",
+			apiRes: &slack.GetConversationHistoryResponse{Messages: []slack.Message{{Msg: slack.Msg{Text: "another", Timestamp: "1512085950.000000"}}, {Msg: slack.Msg{Text: "test", Timestamp: "1512085960.000000"}}, {Msg: slack.Msg{Text: "test", Timestamp: "1512085970.000000"}}, {Msg: slack.Msg{Text: "another", Timestamp: "1512085980.000000"}}}},
+			want:   map[string]*Result{"test": {Details: []*Detail{{t: &c, diff: &b}, {t: &e, diff: &d}}, sum: &d}, "another": {Details: []*Detail{{t: &a, diff: &b}, {t: &f, diff: &g}}, sum: &g}},
 		},
 	}
 
@@ -46,10 +54,10 @@ func TestMakeMap(t *testing.T) {
 					t.Errorf("makeMap() = %v, want %v", (*got)[k], tt.want)
 				}
 				if len((*got)[k].Details) != len(v.Details) {
-					t.Errorf("makeMap() = %v, want %v", len((*got)[k].Details), len(v.Details))
+					t.Errorf("makeMap() len(details) = %v, want %v", len((*got)[k].Details), len(v.Details))
 				}
 				if *((*got)[k].sum) != *(v.sum) {
-					t.Errorf("makeMap() = %v, want %v", *((*got)[k].sum), *(v.sum))
+					t.Errorf("makeMap() sum = %v, want %v", *((*got)[k].sum), *(v.sum))
 				}
 				sort.Slice(v.Details, func(i, j int) bool {
 					return v.Details[i].t.Unix() < v.Details[j].t.Unix()
@@ -59,10 +67,10 @@ func TestMakeMap(t *testing.T) {
 				})
 				for i := 0; i < len(v.Details); i++ {
 					if v.Details[i].t.Unix() != (*got)[k].Details[i].t.Unix() {
-						t.Errorf("makeMap() = %v, want %v", v.Details[i].t.Unix(), (*got)[k].Details[i].t.Unix())
+						t.Errorf("makeMap() unix = %v, want %v", v.Details[i].t.Unix(), (*got)[k].Details[i].t.Unix())
 					}
 					if *(v.Details[i].diff) != *((*got)[k].Details[i].diff) {
-						t.Errorf("makeMap() = %v, want %v", *(v.Details[i].diff), *((*got)[k].Details[i].diff))
+						t.Errorf("makeMap() diff = %v, want %v", *(v.Details[i].diff), *((*got)[k].Details[i].diff))
 					}
 				}
 			}
